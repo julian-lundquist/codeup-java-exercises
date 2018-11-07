81c5aeb8e5eb6f699a5ac173b8722b3657561e9a
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-11-07 12:29:52 -0600

    more java practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,13 +7,38 @@ public class HelloWorld {
         long myNumber = 234L;
         System.out.println(myNumber);
 
-        int x = 5;
-        System.out.println(x++);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+////        int three = (int) "three";
+//        System.out.println(three);
+
+        int x = 4;
+        x += 5;
         System.out.println(x);
 
-        int y = 5;
-        System.out.println(++y);
-        System.out.println(y);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+
     }
 }
 
