75adbd2149c1d764d3a875f39f905051e7568cc6
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-11-07 10:49:03 -0600

    helloworld basic syntax/types/variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println('Hello, World!');
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 3;
+        String myString = ("3.14953");
+        long myNumber = 234L;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
     }
 }
+
