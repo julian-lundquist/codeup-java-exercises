15a0bc3cc2da97a118b1b5159d68ab138e04ccb5
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-11-07 16:53:26 -0600

    started working on java loops/statements

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,15 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i < 15) {
+//            System.out.println(i++);
+//        }
+//        System.out.println(i);
+
+        int i = 2;
+        i = i * i;
+        do {
+            System.out.println(i);
+        } while (i < 1000000);
+    }
+}
