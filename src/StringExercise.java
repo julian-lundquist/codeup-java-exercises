48f5b7f82886d3c4f059a632dd37899f5d587c6b
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-11-08 15:05:51 -0600

    finished the string reassignment console log

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class StringExercise {
+    public static void main(String[] args) {
+        String message;
+
+        message = "We don't need no education";
+        System.out.println(message);
+        message = "We don't need no thought control";
+        System.out.println(message);
+        message = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message);
+        message = "In windows, the main drive is usually C:\\";
+        System.out.println(message);
+        message = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+    }
 }
